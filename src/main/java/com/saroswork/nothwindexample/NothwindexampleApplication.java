904b653265e51be92c23426d437cb5bb58e6a6ee
Sarinho01 <alanvictor.sarinho@gmail.com>
2023-10-25 14:35:49 -0300

    creating spring project

---
+++ src/main/java/com/saroswork/nothwindexample/NothwindexampleApplication.java
@@ -0,0 +1,13 @@
+package com.saroswork.nothwindexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NothwindexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NothwindexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saroswork/nothwindexample/NothwindexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saroswork.nothwindexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NothwindexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

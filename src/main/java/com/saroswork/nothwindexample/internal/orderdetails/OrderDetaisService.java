ae90b32dff10df65988aad59767db2543346d2a9
Sarinho01 <alanvictor.sarinho@gmail.com>
2023-10-29 22:14:00 -0300

    Refactoring project to use VAADIN

--- src/main/java/com/saroswork/nothwindexample/internal/orderdetails/OrderDetaisService.java
+++ src/main/java/com/saroswork/nothwindexample/internal/orderdetails/OrderDetaisService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 
 @Service
-public class OrderDetaisController {
+public class OrderDetaisService {
     @Autowired
     private OrderDetailsRepository orderDetailsRepository;
 

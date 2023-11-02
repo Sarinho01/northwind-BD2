17a04eef0e4fac1f4195a3171c109346987866b6
Sarinho01 <alanvictor.sarinho@gmail.com>
2023-11-02 01:16:54 -0300

    implemented query to filter Orders using orderID

--- src/main/java/com/saroswork/nothwindexample/internal/order/OrderRepository.java
+++ src/main/java/com/saroswork/nothwindexample/internal/order/OrderRepository.java
@@ -1,6 +1,13 @@
 package com.saroswork.nothwindexample.internal.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    @Query("SELECT o FROM Order o " +
+            "WHERE CAST(o.orderID AS string) LIKE concat(:searchTerm,'%')")
+    List<Order> search(@Param("searchTerm") String searchTerm);
 }
